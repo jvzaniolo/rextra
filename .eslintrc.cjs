12117fbd8d117a1b71f7763ab4ce74d390418e2d
João Vitor <jv.zaniolo@gmail.com>
2023-11-01 23:08:22 -0300

    Update dependencies and fix formatting

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+	extends: [
+		'@remix-run/eslint-config',
+		'@remix-run/eslint-config/node',
+		'prettier',
+	],
+	ignorePatterns: ['node_modules', 'build'],
+	rules: {
+		'@typescript-eslint/consistent-type-imports': [
+			'warn',
+			{
+				prefer: 'type-imports',
+				disallowTypeAnnotations: true,
+				fixStyle: 'inline-type-imports',
+			},
+		],
+		'import/no-duplicates': ['warn', { 'prefer-inline': true }],
+		'import/consistent-type-specifier-style': ['warn', 'prefer-inline'],
+		'import/order': [
+			'warn',
+			{
+				alphabetize: { order: 'asc', caseInsensitive: true },
+				groups: [
+					'builtin',
+					'external',
+					'internal',
+					'parent',
+					'sibling',
+					'index',
+				],
+			},
+		],
+	},
+}
